5a35c597e3595c979d85da44562717cbb4cc5549
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-03-02 22:12:34 -0600

    test(constraints)

--- features/steps/two_sum.py
+++ features/steps/two_sum.py
@@ -2,15 +2,39 @@ from aloe import step, world
 
 from apps.leetcode.two_sum import two_sum
 
+constraints = {
+    'length': {'min': 2, 'max': 104},     #    2 <= nums.length <= 104
+    'target': {'min': -109, 'max': 109},  # -109 <= target      <= 109
+    'value': {'min': -109, 'max': 109}    # -109 <= nums[i]     <= 109
+}
+
+
+def assert_is_in_range(x, constraint):
+    minimum = constraint['min']
+    maximum = constraint['max']
+    assert x >= minimum, "min:{expected}, got:{actual}".format(expected=minimum, actual=x)
+    assert x <= maximum, "max:{expected}, got:{actual}".format(expected=maximum, actual=x)
+
+
+validate = {
+    'value': lambda x: assert_is_in_range(x, constraints['value']),
+    'length': lambda length: assert_is_in_range(length, constraints['length']),
+    'target': lambda target: assert_is_in_range(world.target, constraints['target']),
+}
+
 
 @step("an (?P<array>.+) of integers nums")
 def step_impl(self, array):
     world.nums = eval(array)
+    length = len(world.nums)
+    validate['length'](length)
+    map(validate['value'], array)
 
 
 @step("an integer (?P<target>.+)")
 def step_impl(self, target):
     world.target = int(target)
+    validate['target'](world.target)
 
 
 @step("I call two_sum")
--- features/two_sum.feature
+++ features/two_sum.feature
@@ -5,7 +5,7 @@ Feature: Two Sum
 #   * 2 <= nums.length <= 104
 #   * -109 <= nums[i] <= 109
 #   * -109 <= target <= 109
-#   * Only one valid answer exists.
+#   * Only one valid answer exists. # TODO TEST
 
   Scenario Outline: only
      Given an <array> of integers nums
@@ -18,3 +18,5 @@ Feature: Two Sum
   | [2, 7, 11, 15] |      9 |  [0, 1] |    [2, 7]   |
   | [3, 2, 4]      |      6 |  [1, 2] |    [2, 4]   |
   | [3, 3]         |      6 |  [0, 1] |    [3, 3]   |
+# | [60, 70]       |    110 |  [0, 1] |   [60,  70] | # Negative test, as its outside the constraints
+# | [110, 110]     |    240 |  [0, 1] |  [110, 110] | # Negative test, as its outside the constraints
