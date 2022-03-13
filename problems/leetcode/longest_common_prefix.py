07df508b68cde400318e296eb655e3155a035e05
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-03-13 11:11:26 -0500

    docs(problems)
    Added more comments

--- problems/leetcode/longest_common_prefix.py
+++ problems/leetcode/longest_common_prefix.py
@@ -1,13 +1,12 @@
-# https://leetcode.com/problems/longest-common-prefix/
-# Write a function to find the longest common prefix string amongst an array of strings.
-#
-# If there is no common prefix, return an empty string "".
-
 
 def create_filter(prefix):
     return lambda word: word.startswith(prefix)
 
 
+# SRC: https://leetcode.com/problems/longest-common-prefix/
+#
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
 def longest_common_prefix(words):
     length = len(words)
     first_word = words[0]
--- problems/leetcode/summary_ranges.py
+++ problems/leetcode/summary_ranges.py
@@ -6,8 +6,9 @@ def reformat(serie):
     return first if first == last else '->'.join([first, last])
 
 
-# You are given a sorted unique integer array nums.
+# SRC: https://leetcode.com/problems/summary-ranges/
 #
+# You are given a sorted unique integer array nums.
 # Return the smallest sorted list of ranges that cover all the numbers in the array exactly.
 # That is, each element of nums is covered by exactly one of the ranges,
 # and there is no integer x such that x is in one of the ranges but not in nums.
