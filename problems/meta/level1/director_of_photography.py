1cc2d4ba5c214e549ff71bed5234d5f756881f6b
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-04-02 13:49:58 -0500

    fix(director_of_photography)

--- problems/meta/level1/director_of_photography.py
+++ problems/meta/level1/director_of_photography.py
@@ -1,3 +1,10 @@
+from pprint import pprint
+
+# from problems.utils import DEBUG  # FIXME "No module named 'problems'"
+DEBUG = False
+
+
+DIFF_METHOD = 'bounds2'
 
 # A photography set consists of N cells in a row, numbered from 1 to N in order,
 # and can be represented by a string C of length N.
@@ -19,7 +26,6 @@
 #   * a different photographer cell,
 #   * actor cell,
 #   * and/or backdrop cell.
-from pprint import pprint
 
 P = 'P'  # photographer
 A = 'A'  # artist
@@ -36,13 +42,17 @@ _ = '.'  # space
 # There is also this other line
 # "The distance between cells i and j is |i - j| (the absolute value of the difference between their indices)."
 def get_spacing(minimum: int, maximum: int):
-    diff = maximum - minimum  # i.e. 2 -1 = 1
-    return range(0, diff + 1)
-#   return range(minimum, maximum + 1) # TODO ? or XXX?
+    create_range = {
+        'diff': lambda x, y: range(0, y - x + 1),
+        'bounds': lambda x, y: range(x, y + 1),
+        'bounds2': lambda x, y: range(x - 1, y)
+    }
+
+    return create_range[DIFF_METHOD](minimum, maximum)
 
 
-def permute_artistic_photographies(minimum: int, maximum: int) -> list:
-    photographies = []
+def permute_artistic_photos(minimum: int, maximum: int) -> list:
+    photos = []
     for i in get_spacing(minimum, maximum):
         for j in get_spacing(minimum, maximum):
             length = i + j + 1  # actor
@@ -50,41 +60,127 @@ def permute_artistic_photographies(minimum: int, maximum: int) -> list:
             array.insert(0, P)
             array[i + 1] = A
             array.append(B)
-            photographies.append(array)
+            photos.append(array)
 
     # NOTE: I could do this in the for loop above
     # But this is a more pythonic way
-    for array in photographies.copy():
+    for array in photos.copy():
         inverted = array[::-1]
-        photographies.append(inverted)
+        photos.append(inverted)
 
-    return photographies
+    return photos
 
 
-def permute_scenarios(miminum: int, maximum: int, length: int) -> list:
-    photographies = permute_artistic_photographies(miminum, maximum)
+def permute_scenarios(length: int, photos: list) -> list:
     results = []
-    for photography in photographies:
-        _len = len(photography)
+    for photo in photos:
+        _len = len(photo)
         diff = length - _len
         for i in range(0, diff +1):
             left = [_ for idx in range(0, i)]
             right = [_ for idx in range(0, diff - i)]
-            pprint((left, photography, right))
-            expanded = left + photography + right
+            if DEBUG is True:
+                pprint((left, photo, right))
+
+            expanded = left + photo + right
             results.append(expanded)
 
     return results
 
 
+def expand_mask(mask:str) -> list:
+    # '.PBAAP.B'
+    #
+    # '.P_A__.B'
+    # '.P__A_.B'
+    # '._BA_P._'
+    # '._B_AP._'
+    chars = list(mask)
+    indexes = {
+        'P': [],
+        'A': [],
+        'B': []
+    }
 
-# def getArtisticPhotographCount(N: int, C: str, X: int, Y: int) -> int:
-#     mask = C
-#     minimum = X
-#     maximum = Y
-#
+    # FIXME simplify
+    for idx, char in enumerate(chars):
+        for element in [P, A, B]:
+            if element == char:
+                indexes[element].append(idx)
 
+    filters = []
+    for p in indexes[P]:
+        for a in indexes[A]:
+            for b in indexes[B]:
+                if (p > a) and (b > a):
+                    continue
+
+                if (p < a) and (b < a):
+                    continue
+
+                array = [_ for idx in range(0, len(chars))]
+                array[p] = P
+                array[a] = A
+                array[b] = B
+                string = ''.join(array)
+                filters.append(string)
+
+    return filters
+
+
+def pretty_print(items: list) -> None:
+    strings = [''.join(item) for item in items]
+    pprint(strings)
+
+
+def get_artistic_photos(mask:str, minimum:int, maximum:int) -> list:
+    photos = permute_artistic_photos(minimum, maximum)
+    scenarios = permute_scenarios(len(mask), photos)
+    filters = expand_mask(mask)
+    filtered = [
+        photo
+        for photo in scenarios
+        if ''.join(photo) in filters]
+
+    if DEBUG:
+        pretty_print(filtered)
+
+    return filtered
+
+
+def getArtisticPhotographCount(N: int, C: str, X: int, Y: int) -> int:
+    assert len(C) == N, f'mask:{C} is NOT of size {N}'
+    valid_photos = get_artistic_photos(C, X, Y)
+    return len(valid_photos)
+
+
+def run(length:int, mask:str, minimium: int, maximum: int, expected: int) -> None:
+    result = getArtisticPhotographCount(length, mask, minimium, maximum)
+    assert result == expected, f'expected:{expected}, got:{result}'
+
+
+def run_all():
+    # Sample test case #1
+    # * N=5, C=APABA
+    # * X=1, Y=2
+    #
+    # Expected Return Value = 1
+    run(5, 'APABA', 1, 2, expected=1)
+
+    # Sample test case #2
+    # * N=5, C=APABA
+    # * X=2, Y=3
+    #
+    # Expected Return Value = 0
+    run(5, 'APABA', 2, 3, expected=0)
+
+    # Sample test case #3
+    # N=8, C=.PBAAP.B
+    # X=1, Y=3
+    #
+    # Expected Return Value = 3
+    run(8, '.PBAAP.B', 1, 3, expected=3)
 
 
 if __name__ == '__main__':
-    photographies = permute_scenarios(1, 2, 8)
+    run_all()
