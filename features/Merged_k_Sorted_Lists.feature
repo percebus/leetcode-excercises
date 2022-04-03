b1f6fd9947e584d4ba34bba96310528ebf99e8cb
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-04-02 19:08:42 -0500

    test(Merge k Sorted Lists)

--- features/Merged_k_Sorted_Lists.feature
+++ features/Merged_k_Sorted_Lists.feature
@@ -9,11 +9,38 @@ Feature: Merge k Sorted Lists
   #  * lists[i] is sorted in ascending order.
   #  * The sum of lists[i].length will not exceed 10^4
 
-  Scenario Outline: _
+  Scenario Outline: Simple
     Given an <array> of k linked-lists lists
 #     And each linked-list is sorted in ascending order
      When I call mergeKLists
      Then it returns all the linked-lists merged into one sorted linked-list as <result>
   Examples:
-  |       array       |     result      |
-  | 1→4→5, 1→3→4, 2→6 | 1→1→2→3→4→4→5→6 |
+  |     array          |    result     |
+  | [ {1} ]            | {1}           |
+  | [ {1→2} ]          | {1→2}         |
+  | [ {1→2→3} ]        | {1→2→3}       |
+  | [ {1→2→3→4} ]      | {1→2→3→4}     |
+  | [ {1→2→3→4→5} ]    | {1→2→3→4→5}   |
+  | [ {1→2→3→4→5→6} ]  | {1→2→3→4→5→6} |
+
+
+  Scenario Outline: Complex
+    Given an <array> of k linked-lists lists
+#     And each linked-list is sorted in ascending order
+     When I call mergeKLists
+     Then it returns all the linked-lists merged into one sorted linked-list as <result>
+  Examples:
+  |          array              |       result      |
+  | [ {1→2}, {3→4}, {5→6} ]     | {1→2→3→4→5→6}     |
+  | [ {1→4→5}, {1→3→4}, {2→6} ] | {1→1→2→3→4→4→5→6} |
+
+
+  Scenario Outline: Empty
+    Given an <array> of k linked-lists lists
+#     And each linked-list is sorted in ascending order
+     When I call mergeKLists
+     Then it returns all the linked-lists merged into one sorted linked-list as <result>
+  Examples:
+  |  array  | result |
+  |   []    |   {}   |
+  | [ {} ]  |   {}   |
--- features/steps/Merged_k_Sorted_Lists.py
+++ features/steps/Merged_k_Sorted_Lists.py
@@ -3,17 +3,43 @@ from problems.leetcode.list_node import list_to_nodes, nodes_to_list
 from problems.leetcode.hard.merge_k_sorted_lists import mergeKLists
 
 
-def parse(string):
-    nums = [
-        int(num)
-        for num in string.split('→')]
+def remove_brackets(string):
+    _string = string
+    _string = _string.replace('{', '')
+    _string = _string.replace('}', '')
+    return _string
+
+
+def parse_chain(string):
+    if string == '{}':
+        return []
+
+    _string = remove_brackets(string)
+    return [int(num) for num in _string.split('→')]
+
+
+def parse_nodes(string):
+    if string == '':
+        return None
+
+    nums = parse_chain(string)
     return list_to_nodes(nums)
+
+
+def parse_array(string):
+    if string == '[]':
+        return []
+
+    _string = string
+    _string = _string.replace('[ ', '')
+    _string = _string.replace(' ]', '')
+    strings = _string.split(', ')
+    return list(map(parse_nodes, strings))
 
 
 @step("an (?P<array>.+) of k linked-lists lists")
 def step_impl(self, array):
-    strings = array.split(', ')
-    world.nodes = map(parse, strings)
+    world.nodes = parse_array(array)
 
 
 @step("I call mergeKLists")
@@ -23,11 +49,10 @@ def step_impl(self):
 
 @step("it returns all the linked-lists merged into one sorted linked-list as (?P<result>.+)")
 def step_impl(self, result):
-    _expected = result
-    expected = [
-        int(num)
-        for num in result.split('→')]
+    expected = parse_chain(result)
+    _expected = '→'.join([str(num) for num in expected])
 
     actual = nodes_to_list(world.result)
     _actual = '→'.join([str(num) for num in actual])
+
     assert _actual == _expected, f'expected:{expected}, got:{actual}'
