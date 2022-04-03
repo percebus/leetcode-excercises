3028a19fff0a703112e17393248b25ba74f3561d
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-04-03 12:00:41 -0500

    test(Longest Palindromic Substring)

---
+++ features/steps/Longest_Palindromic_Substring.py
@@ -0,0 +1,19 @@
+from aloe import step, world
+
+from problems.leetcode.medium.longest_palindromic_substring import longestPalindrome
+
+
+@step("a (?P<string>.+) s")
+def step_impl(self, string):
+    world.string = string
+
+
+@step("I call longestPalindrome")
+def step_impl(self):
+    world.result = longestPalindrome(world.string)
+
+
+@step("it returns the longest (?P<palindrome>.+) in the string")
+def step_impl(self, palindrome):
+    expected = palindrome
+    assert world.result == expected, f'expected:{expected}, got:{world.result}'
