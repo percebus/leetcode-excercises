5646521a8a437fe26c637720f0765a29211b9381
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-04-11 09:23:00 -0500

    test(Sum Even Fibonacci Numbers)

---
+++ features/steps/Sum_Even_Fibonacci_Numbers.py
@@ -0,0 +1,18 @@
+from aloe import step, world
+from problems.euler.even_fibonacci_numbers import sum_evens
+
+
+@step("a (?P<limit>.+) number")
+def step_impl(self, limit):
+    world.limit = int(limit)
+
+
+@step("I call sum_evens")
+def step_impl(self):
+    world.result = sum_evens(world.limit)
+
+
+@step("I get the (?P<total>.+) sum of all even numbers inside the Fibonacci series, up until that limit")
+def step_impl(self, total):
+    expected = int(total)
+    assert world.result == expected, f'expected:{expected}, got:{world.result}'
